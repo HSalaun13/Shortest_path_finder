0a03559cc472b5636eb759ab7a247a228c0ffefc
HSalaun13 <A01833679@tec.mx>
2026-09-24 18:12:50 -0600

    Refactor Dist_menu_tests.py with map display

--- Dist_menu_tests.py
+++ Dist_menu_tests.py
@@ -1,18 +1,30 @@
+Map = [
+    ["S", ".", ".", ".", "&"],
+    [".", "&", ".", "&", "."],
+    ["&", "&", ".", ".", "."],
+    [".", ".", "&", ".", "D"]
+]
+"""Example of a map used in the game with a start(S), a destination(D), Obstacles (&) and The free path (.)"""
+
 def calcul_dist(x1, y1, x2, y2):
-    """Calculate the distance between two points"""
+    """Calculate the distance between two points X1, and X2"""
     dist = abs(x2 - x1) + abs(y2 - y1)
     return dist
 
+def Display(Map):
+    """This function display the map."""
+    for row in Map:
+        print(row)
 
 def menu():
-    """Menu display"""
+    """Menu function to let the user choose the option he whants """
     print("1. Calcul of the distance")
     print("2. Valid position ?")
     print("3. Exit")
 
 
 def tests():
-    """Test Functions."""
+    """This function tests some cases"""
     if calcul_dist(0, 0, 3, 4) == 7:
         print("Test 1 --> ok")
     else:
